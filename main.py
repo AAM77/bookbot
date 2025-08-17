a94fc38aa601c434142b6206c838087987d7a5b1
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2025-08-17 07:32:32 -0400

    Make path to text more dynamic with error handling

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import sys
+
 from stats import count_num_words
 from stats import count_num_chars
 from stats import get_sorted_dictionaries
 
 def main() -> None:
-    text_path: str = "books/frankenstein.txt"
-    book_text: str = get_book_text(text_path)
-    word_count: int = count_num_words(book_text)
-    character_counts: dict = count_num_chars(book_text)
-    print(f"{word_count} words found in the document")
-    print_report(text_path, word_count, character_counts)
+    try:
+        if len(sys.argv) == 2:
+            text_path: str = sys.argv[1]
+            book_text: str = get_book_text(text_path)
+            word_count: int = count_num_words(book_text)
+            character_counts: dict = count_num_chars(book_text)
+            print(f"{word_count} words found in the document")
+            print_report(text_path, word_count, character_counts)
+        else:
+            print("Usage: python3 main.py <path_to_book>")
+            sys.exit(1)
+    except Exception as e:
+        print(f"Error: {e}")
+    
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
