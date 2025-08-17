d9ec57cc00847b4f02bb630417d340244c17a2a0
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2025-08-17 05:40:47 -0400

    Create methods to sort and print report

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from stats import count_num_words
 from stats import count_num_chars
+from stats import get_sorted_dictionaries
 
 def main() -> None:
-    book_text = get_book_text("books/frankenstein.txt")
-    word_count = count_num_words(book_text)
+    text_path: str = "books/frankenstein.txt"
+    book_text: str = get_book_text(text_path)
+    word_count: int = count_num_words(book_text)
+    character_counts: dict = count_num_chars(book_text)
     print(f"{word_count} words found in the document")
-    print(count_num_chars(book_text))
+    print_report(text_path, word_count, character_counts)
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
+
+def print_report(text_path: str, word_count: int, character_counts: dict) -> None:
+    sorted_character_dictionaries: list = get_sorted_dictionaries(character_counts)
+    header_text: str = f"""
+    ============ BOOKBOT ============
+    Analyzing book found at {text_path}...
+    ----------- Word Count ----------
+    Found {word_count} total words
+    --------- Character Count -------
+    """
+    ending_line: str = "============= END ==============="
+    
+    print(header_text)
+    for character_dictionary in sorted_character_dictionaries:
+        if character_dictionary["name"].isalpha():
+            print(f"{character_dictionary["name"]}: {character_dictionary["num"]}")
+    print(ending_line)
+    
+
 
 main()
--- stats.py
+++ stats.py
@@ -13,4 +13,14 @@ def count_num_chars(file_contents: str) -> dict:
             char_counts[char] = 1
 
     return char_counts
-    
+
+def get_character_dictionaries_list(character_counts: dict) -> list:
+    return [{"name": character, "num": count} for character, count in character_counts.items()]
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_dictionaries(character_counts: dict) -> list:
+    char_dictionaries_list: list = get_character_dictionaries_list(character_counts)
+    char_dictionaries_list.sort(reverse=True, key=sort_on)
+    return char_dictionaries_list
