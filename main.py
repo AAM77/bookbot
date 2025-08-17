b3d2f6fafcfbf024862aa144e29b705bd806c462
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2025-08-17 04:56:20 -0400

    Add a method to return character counts.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from stats import count_num_words
+from stats import count_num_chars
 
 def main() -> None:
     book_text = get_book_text("books/frankenstein.txt")
     word_count = count_num_words(book_text)
     print(f"{word_count} words found in the document")
+    print(count_num_chars(book_text))
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def count_num_words(file_contents: str) -> int:
     word_list: list = file_contents.split()
     word_count: int = len(word_list)
     return word_count
+
+def count_num_chars(file_contents: str) -> dict:
+    char_counts: dict[str, int] = {}
+    lower_cased_contents: str = file_contents.lower()
+    for char in lower_cased_contents:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+
+    return char_counts
+    
